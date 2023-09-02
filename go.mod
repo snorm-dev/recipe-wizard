3bc537aea93b88f112425b35ce52718e34940043
Seth Norman <san72@cornell.edu>
2023-09-02 18:55:24 +0000

    FIll in recipe schema

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/snorman7384/recipe-wizard
 go 1.20
 
 require (
-	github.com/go-chi/chi/v5 v5.0.10 // indirect
-	github.com/go-chi/cors v1.2.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
 )
--- sql/schema/20230901220843_recipes.sql
+++ sql/schema/20230901220843_recipes.sql
@@ -2,6 +2,14 @@
 -- +goose StatementBegin
 CREATE TABLE recipes (
 	id VARCHAR(36) PRIMARY KEY,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
+	name TEXT NOT NULL,
+	description TEXT,
+	url VARCHAR(512) CHARACTER SET 'ascii' COLLATE 'ascii_general_ci',
+	prep_time TIME,
+	cook_time TIME,
+	cool_time TIME
 );
 -- +goose StatementEnd
 
