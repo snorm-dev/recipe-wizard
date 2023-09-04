e79ab8ffa6c5ff6743b079c6c08b937c5f19891b
Seth Norman <san72@cornell.edu>
2023-09-04 00:49:28 +0000

    Added recipe queries

---
+++ sql/queries/recipes.sql
@@ -0,0 +1,10 @@
+-- name: CreateRecipe :exec
+INSERT INTO recipes(id, created_at, updated_at, name, description, url, prep_time, cook_time, cool_time)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+
+-- name: GetRecipe :one
+SELECT * FROM recipes
+WHERE id = ?;
+
+-- name: GetRecipes :many
+SELECT * FROM recipes;
